314131cdba757105da0d60dc5ddfe8fa46d9c6cc
Gautier Caroff <gautier.caroff2023@campus-eni.fr>
2023-08-10 10:02:27 +0200

    création du depot

---
+++ src/main/java/fr/eni/bidon/BidonApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.bidon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidonApplication.class, args);
+	}
+
+}
